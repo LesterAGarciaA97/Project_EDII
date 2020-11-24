3f65607e03d2bc6a54011c321c115828cb237db5
HectorZetino <h.rzc.zetino@hotmail.es>
2020-11-24 16:13:46 -0600

    Metodos manejo de la base , pendiente comentarios para mejor compresion

--- Solucion/ApiChat/ApiChat/DataServices/UserServices.cs
+++ Solucion/ApiChat/ApiChat/DataServices/UserServices.cs
@@ -1,11 +1,15 @@
 ﻿using ApiChat.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Driver;
 using MongoDB.Driver.GridFS;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ApiChat.DataServices
@@ -77,6 +81,85 @@ namespace ApiChat.DataServices
             }
             var User1 = _Users.Find(EmisorFilter).ToList();
             return User1[0].Username + "." + User1[0].Id;
+        }
+        public bool UpdateMessageReceptor(string IdReceptor, string UsernameComEmisor, Msg objMensaje) {
+            var UserFilter = Builders<User>.Filter.Eq("Id", IdReceptor);
+            if (_Users.Find(UserFilter).ToList().Count == 1)
+            {
+                var ConversacionesFilter = Builders<User>.Filter.Eq("Id", IdReceptor) & Builders<User>.Filter.Eq("Conversaciones", UsernameComEmisor);
+                if (_Users.Find(ConversacionesFilter).ToList().Count == 0)
+                {
+                    var UpdateConversacion = Builders<User>.Update.Push("Conversaciones", UsernameComEmisor);
+                    _Users.UpdateOne(UserFilter, UpdateConversacion);
+                }
+                var UpdateM = Builders<User>.Update.Push("Mensajes", objMensaje);
+                _Users.UpdateOne(UserFilter, UpdateM);
+
+                return true;
+            }
+            
+            return false;
+        }
+
+        public List<Msg> GetMessages(string IdEmisor, string IdReceptor) {
+            var Usuario = _Users.Find(user => user.Id == IdEmisor).FirstOrDefault();
+            var Enviados = Usuario.Mensajes.FindAll(mensaje => (mensaje.Emisor == IdEmisor && mensaje.Receptor == IdReceptor) || (mensaje.Emisor == IdReceptor && mensaje.Receptor == IdEmisor));
+            return Enviados;
+        }
+
+        public List<string> GetConversations(string idEmisor) {
+            var usuario = _Users.Find(user => user.Id == idEmisor).FirstOrDefault();
+            return usuario.Conversaciones;
+        }
+
+        //Procesos de autenticacion y tambien inicio de sesion
+        public Jwt Authenticate(string UserName, string Password) {
+            var user = _Users.Find(_User => _User.Username == UserName && _User.Contrasena == Password).FirstOrDefault();
+            if (user == null)
+            {
+                return null;
+            }
+            var _jwt = new Jwt();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_AppSettings.Secret);
+            var TokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(3),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var Token = tokenHandler.CreateToken(TokenDescriptor);
+            _jwt.Token = tokenHandler.WriteToken(Token);
+            return _jwt;
+        }
+
+        public bool SendDocuments(string FileName, byte[] FileBytes) {
+            GridFSBucket GridFs = new GridFSBucket(_db);
+            var ObjectId = GridFs.UploadFromBytesAsync(FileName, FileBytes);
+            return true;
+        }
+
+        public Document SendDocuments(Document document)
+        {
+            _Documents.InsertOne(document);
+
+            return document;
+        }
+
+        public List<Document> GetDocuments(string usId)
+        {
+            var listaDocs = _Documents.Find(doc => doc.EmisorId == usId || doc.ReceptorId == usId).ToList();
+
+            return listaDocs;
+        }
+
+        public Document DownloadDocument(string fileName)
+        {
+            var doc = _Documents.Find(docX => docX.DocNombre == fileName).FirstOrDefault();
+
+            return doc;
         }
     }
 }
