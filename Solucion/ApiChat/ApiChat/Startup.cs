73152e6d8e279ee468282ff9e18a9f7d52e9fa73
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2020-11-24 19:59:47 -0600

    Se modificó Startup para trabajó de autenticación y conexión MVC

--- Solucion/ApiChat/ApiChat/Startup.cs
+++ Solucion/ApiChat/ApiChat/Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using ApiChat.DataServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ApiChat
 {
@@ -25,7 +29,35 @@ namespace ApiChat
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            //services.AddControllers();
+            services.AddCors();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            //Obtención de una Key secreta para la encripción utilizando JWT como método de autenticación web
+            var appSettingsSection = Configuration.GetSection("AppConfiguration");
+            services.Configure<AppConfiguration>(appSettingsSection);
+
+            //Aquí se utiliza el método de autenticación para las sesiones
+            var AppConfiguration = appSettingsSection.Get<AppConfiguration>();
+            var key = Encoding.ASCII.GetBytes(AppConfiguration.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            services.AddScoped<UserServices>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +68,7 @@ namespace ApiChat
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            /*app.UseHttpsRedirection();
 
             app.UseRouting();
 
@@ -45,7 +77,16 @@ namespace ApiChat
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
+            });*/
+
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            );
+
+            app.UseAuthentication();
+            app.UseMvc();
         }
     }
 }
