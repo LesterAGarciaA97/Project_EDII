371f7897fdd246bc08a2b70b7ab2209074e4b884
HectorZetino <h.rzc.zetino@hotmail.es>
2020-11-24 18:12:26 -0600

    Agregando Validaciones, pendiente controlador

---
+++ Solucion/ApiChat/ApiChat/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiChat.Controllers
+{
+    [Authorize]
+    [Route("RocketChat/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
